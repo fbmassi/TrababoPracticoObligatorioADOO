b4d495d37535c0c1813ddc4db2d2094b472ed7aa
Franco <francomassi1@gmail.com>
2024-11-09 20:21:16 -0300

    depsndencias

--- GimnasioADOO/src/main/java/interfaces/IMedidorAdapter.java
+++ GimnasioADOO/src/main/java/interfaces/IMedidorAdapter.java
@@ -1,9 +1,18 @@
 package interfaces;
 
+<<<<<<< Updated upstream
 public interface IMedidorAdapter {
     void medirPeso();
 
     float medirMasaMuscular();
 
     public float medirGrasaCorporal();
+=======
+import java.util.List;
+
+public interface IMedidorAdapter {
+
+    List<Float> medirEstadoFisico();
+    
+>>>>>>> Stashed changes
 }
